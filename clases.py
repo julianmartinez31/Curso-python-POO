77916441e855bcf0eef79807416425346eee4b75
Julian Orosco <julianorosco37@gmail.com>
2022-05-02 21:42:44 -0500

    Primer clase del curso

--- clases.py
+++ clases.py
@@ -1,20 +1,32 @@
-# definicion de clase 
+# # definicion de clase 
 
-class nombre_de_la_clase(super_clase):
+# class nombre_de_la_clase(super_clase):
 
-    # a toda funcion dentro de una clase se le llama metodo
-    def __init__(self, parametros):
-        pass
-        #expresiones del metodo
+#     # a toda funcion dentro de una clase se le llama metodo
+#     def __init__(self, parametros):
+#         pass
+#         #expresiones del metodo
 
-# EJEMPLO
+# # EJEMPLO
 
-class persona:
-    def __init__(self, nombre, edad):
-        self.nombre = nombre
-        self.edad = edad
+def run():
 
-    def saluda(self, otra_persona):
-        return f'Hola {otra_persona.nombre, me llamo {self.nombre}.'
+    class Persona:
+        def __init__(self, nombre, edad):
+            self.nombre = nombre
+            self.edad = edad
+
+        def saluda(self, otra_persona):
+            return f'Hola {otra_persona.nombre}, me llamo {self.nombre}.'
+
+
+    david = Persona("david", 35)
+    erika = Persona("erika", 44)
+
+    print(david.saluda(erika))
+
+if __name__ == '__main__':
+    run()
+
         
          
