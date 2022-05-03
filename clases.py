38083cf21ce22ec83eeb5881359b5e59532e981f
Julian Orosco <julianorosco37@gmail.com>
2022-05-02 23:08:15 -0500

    Ejemplo de distancia de 2 coordenadas utilizando clases

--- clases.py
+++ clases.py
@@ -9,25 +9,42 @@
 
 # # EJEMPLO
 
-def run():
+#def run():
 
-    class Persona:
-        def __init__(self, nombre, edad):
-            self.nombre = nombre
-            self.edad = edad
+#     class Persona:
+#         def __init__(self, nombre, edad):
+#             self.nombre = nombre
+#             self.edad = edad
 
-        def saluda(self, otra_persona):
-            return f'Hola {otra_persona.nombre}, me llamo {self.nombre}.'
+#         def saluda(self, otra_persona):
+#             return f'Hola {otra_persona.nombre}, me llamo {self.nombre}.'
 
 
-    david = Persona("david", 35)
-    erika = Persona("erika", 44)
+#     david = Persona("david", 35)
+#     erika = Persona("erika", 44)
 
-    print(david.saluda(erika))
+#     print(david.saluda(erika))
 
+class coordenada:
+
+    def __init__(self,cordx,cordy):
+        self.cordx = cordx
+        self.cordy = cordy
+
+    def diff(self,otra_cord):
+
+        difx = (self.cordx - otra_cord.cordx)**2
+        dify = (self.cordy - otra_cord.cordy)**2
+
+        return  (difx + dify)**0.5
+ 
     
-if __name__ == '__main__':
-    run()
 
-        
+if __name__ == '__main__':
+   
+    punto1 = coordenada(3,30)
+    punto2 = coordenada(4,8)
+
+    print(str(punto1.diff(punto2)))
+
          
